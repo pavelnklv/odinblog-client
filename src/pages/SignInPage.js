3af5636bb098ce8128fd7e93ce6fa6ddeb111343
nikolaev_pavel <nikolaev_pavel@aol.com>
2021-01-17 15:31:13 +0300

    add errors

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,43 +1,65 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/authContext';
+import emailValid from '../utils/emailValid';
 
 export default function SignInPage() {
+  const { signIn } = useContext(AuthContext);
+
   const [userValues, setUserValues] = useState({
     email: '', password: '',
   });
+  const [errors, setErrors] = useState({
+    email: null, password: null,
+  })
 
   const onChange = e => setUserValues({ ...userValues, [e.target.name]: e.target.value });
 
   const onSubmit = e => {
     e.preventDefault()
-    alert(JSON.stringify(userValues))
+    
+    setErrors({ email: null, password: null })
+    if (!emailValid(userValues.email)) {
+      setErrors({ ...errors, email: 'Please provide a valid email.'})
+    }
+    if (userValues.password.trim() === '') {
+      setErrors({ ...errors, password: 'Please provide a password.'})
+    }
+
+    signIn(userValues)
   };
 
   return (
     <div className="row justify-content-center">
       <div className="col-12 col-lg-6">
         <h2 className="text-center mb-5">Sign In</h2>
-        <form className="needs-validation" onSubmit={onSubmit}>
+        <form className="needs-validation" onSubmit={onSubmit} noValidate>
           <div className="mb-3">
             <label className="form-label" htmlFor="email">Email</label>
             <input
-              className="form-control"
+              className={`form-control ${errors.email ? 'is-invalid' : ''}`}
               type="email"
               name="email"
               onChange={onChange}
               value={userValues.email}
             />
+            {errors.email && (
+              <div className="invalid-feedback">{errors.email}</div>
+            )}
           </div>
 
           <div className="mb-3">
             <label className="form-label" htmlFor="password">Password</label>
             <input
-              className="form-control"
+              className={`form-control ${errors.password ? 'is-invalid' : ''}`}
               type="password"
               name="password"
               onChange={onChange}
               value={userValues.password}
             />
+            {errors.password && (
+              <div className="invalid-feedback">{errors.password}</div>
+            )}
           </div>
 
           <div className="d-grid">
