037aeb5036cf74652cc689934cf551813d2f9a67
nikolaev_pavel <nikolaev_pavel@aol.com>
2021-01-17 18:58:41 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useContext } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { AuthContext } from './context/authContext';
+
+import Loader from './components/Loader';
 import Navbar from './components/Navbar';
 
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
 
 function App() {
+  const { loading } = useContext(AuthContext)
+
+  if (loading) return <Loader />
+
   return (
     <BrowserRouter>
       <Navbar />
