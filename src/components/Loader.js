a47d5e2976f72263645cc840589b52dcf9ec5fe7
nikolaev_pavel <nikolaev_pavel@aol.com>
2021-01-17 18:13:43 +0300

    create Loader

---
+++ src/components/Loader.js
@@ -0,0 +1,15 @@
+export default function Loader() {
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="spinner-grow text-primary">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+      <div className="spinner-grow text-primary">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+      <div className="spinner-grow text-primary">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+}
