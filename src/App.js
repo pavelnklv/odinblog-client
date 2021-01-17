bd0f376c0764708a82e6008b012ddcdfa76f6854
nikolaev_pavel <nikolaev_pavel@aol.com>
2021-01-17 14:53:06 +0300

    add SignUpPage and SignInPage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Navbar from './components/Navbar';
 
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
+
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +12,12 @@ function App() {
         <Switch>
           <Route path="/" exact>
             <h1>Home</h1>
+          </Route>
+          <Route path="/sign-up" exact>
+            <SignUpPage />
+          </Route>
+          <Route path="/sign-in" exact>
+            <SignInPage />
           </Route>
         </Switch>
       </div>
