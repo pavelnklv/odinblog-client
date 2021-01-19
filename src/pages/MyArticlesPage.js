5bc959bea468a419c9da24673d33efe927b8c7f6
nikolaev_pavel <nikolaev_pavel@aol.com>
2021-01-19 20:23:41 +0300

    add deletebtn func

--- src/pages/MyArticlesPage.js
+++ src/pages/MyArticlesPage.js
@@ -1,6 +1,5 @@
 import { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import ArticleItem from '../components/ArticleItem';
 import Loader from '../components/Loader';
 import { AuthContext, JWT_KEY } from '../context/authContext';
 
@@ -9,6 +8,17 @@ export default function MyArticlesPage() {
   const [activeTab, setActiveTab] = useState('drafts');
   const [loading, setLoading] = useState(true);
   const [articles, setArticles] = useState([]);
+
+  const onDeleteClick = async articleSlug => {
+    const res = await fetch(`/api/articles/${articleSlug}`, {
+      method: 'DELETE',
+      headers: { 'Authorization': `Bearer ${localStorage.getItem(JWT_KEY)}`}
+    });
+    if (res.ok) {
+      const newArticles = articles.filter(article => article.slug !== articleSlug)
+      setArticles(newArticles)
+    }
+  }
 
   useEffect(() => {
     if (activeTab === 'drafts') {
@@ -89,6 +99,7 @@ export default function MyArticlesPage() {
                   <i
                     role="button"
                     className="text-danger mx-1"
+                    onClick={() => onDeleteClick(article.slug)}
                   >
                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
                       <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
